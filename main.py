40067b142724d1d81481a145f128461728102fbf
rahulrameshm0 <iamrahulm23@gmail.com>
2026-06-12 16:03:37 +0530

    create yaml file and implemented a Kafka producer to send messages to a Kafka topic.

---
+++ main.py
@@ -0,0 +1,33 @@
+from confluent_kafka import Producer
+import uuid
+import json
+producer_config = {
+    'bootstrap.servers': 'localhost:9092'
+}
+producer = Producer(producer_config)
+
+def delivery_report(err, msg):
+    if err:
+        print(f"❌ Delivery Failed: {err}")
+    else:
+        print(f"✅ Delivered {msg.value().decode('utf-8')}")
+        print(f"✅ Delivered to {msg.topic()}: Partition {msg.partition()}: at offset {msg.offset()}")
+
+order = {
+    "order_id": str(uuid.uuid4()),
+    "user":"rahul",
+    "item":"Pizza",
+    "quantity": 2
+}
+
+
+value = json.dumps(order).encode("utf-8")
+producer.produce(topic="orders",
+                 value=value,
+                 callback=delivery_report
+                 )
+
+producer.flush()
+
+
+
